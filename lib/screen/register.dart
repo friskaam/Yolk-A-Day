e1018c1e11c876ddfcc990791c646d2befc5944c
manuel_yosia <manuelyosia44@gmail.com>
2024-02-04 11:32:16 +0700

    add login and register screen

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -13,35 +13,168 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
+        body: SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.all(20),
+        padding: EdgeInsets.symmetric(vertical: 0, horizontal: 56),
         child: Column(
           children: [
-            Row(),
-            Row(
-              children: [
-                Form(
-                  key: _formKey,
-                  child: Column(children: [
-                    Expanded(
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Username',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please input username';
-                          }
-
-                          return null;
-                        },
-                      ),
-                    )
-                  ]),
-                ),
-              ],
+            SizedBox(
+              height: 269,
+              child: Stack(
+                alignment: AlignmentDirectional.bottomCenter,
+                children: [
+                  OverflowBox(
+                    maxWidth: 600,
+                    child: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(300),
+                              bottomRight: Radius.circular(300)),
+                          color: Color.fromARGB(255, 248, 192, 56)),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 60,
+                    child: Text(
+                      "One step closer",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Outfit',
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  )
+                ],
+              ),
             ),
+            SizedBox(
+              height: 29,
+            ),
+            Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 248, 192, 56),
+                                  width: 3,
+                                  style: BorderStyle.solid)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 248, 192, 56),
+                                  width: 3,
+                                  style: BorderStyle.solid)),
+                          labelText: 'Username',
+                          labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.normal)),
+                      style: TextStyle(),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 248, 192, 56),
+                                  width: 3,
+                                  style: BorderStyle.solid)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 248, 192, 56),
+                                  width: 3,
+                                  style: BorderStyle.solid)),
+                          labelText: 'Email',
+                          labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.normal)),
+                      style: TextStyle(),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 248, 192, 56),
+                                  width: 3,
+                                  style: BorderStyle.solid)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 248, 192, 56),
+                                  width: 3,
+                                  style: BorderStyle.solid)),
+                          labelText: 'Password',
+                          labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.normal)),
+                      style: TextStyle(),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 248, 192, 56),
+                                  width: 3,
+                                  style: BorderStyle.solid)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 248, 192, 56),
+                                  width: 3,
+                                  style: BorderStyle.solid)),
+                          labelText: 'Confirm Password',
+                          labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.normal)),
+                      style: TextStyle(),
+                    ),
+                    SizedBox(
+                      height: 47,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(255, 248, 192, 56)),
+                          onPressed: () {},
+                          child: Text('Register')),
+                    )
+                  ],
+                ))
           ],
         ),
       ),
