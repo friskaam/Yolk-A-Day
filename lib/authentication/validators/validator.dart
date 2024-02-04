9477cedb1d0b67a79a033b4ae6179f41ffb652c7
Friska Adisti Mahardini <friskamahardini@gmail.com>
2024-02-04 18:50:01 +0700

    add form validator

---
+++ lib/authentication/validators/validator.dart
@@ -0,0 +1,56 @@
+class FormValidator {
+  static String? validateEmptyText(String? fieldName, String? value) {
+    if (value == null || value.isEmpty) {
+      return '$fieldName is required';
+    }
+    return null;
+  }
+
+  static String? validateUsername(String? value) {
+
+    return null;
+  }
+
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+
+    final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Invalid email address.';
+    }
+    return null;
+  }
+
+  static validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required.';
+    }
+
+    if (value.length < 8) {
+      return 'Password must be at least 8 characters long.';
+    }
+
+    if (!value.contains(RegExp(r'[A-Z]'))) {
+      return 'Password must contain at least one uppercase letter.';
+    }
+
+    if (!value.contains(RegExp(r'[0-9]'))) {
+      return 'Password must contain at least one number.';
+    }
+
+    if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+      return 'Password must contain at least special character.';
+    }
+    return null;
+  }
+
+  static String? validateConfirmPassword(String? originalPassword, String? confirmPassword) {
+    if (confirmPassword != originalPassword) {
+      return 'Passwords do not match.';
+    }
+    return null;
+  }
+}
