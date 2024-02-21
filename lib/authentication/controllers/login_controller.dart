657163376ab0a3406222f38caab701e8a6f4084c
Friska Adisti Mahardini <friskamahardini@gmail.com>
2024-02-21 16:29:31 +0700

    feat: login authentication

--- lib/authentication/controllers/login_controller.dart
+++ lib/authentication/controllers/login_controller.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:yolk_a_day/navigation_menu.dart';
+
+import '../../data/repositories/auth/auth_repository.dart';
+import '../../data/repositories/auth/network_manager.dart';
+import '../../loader/custom_loader.dart';
 
 class LoginController extends GetxController {
   static LoginController get instance => Get.find();
 
+  final localStorage = GetStorage();
+  final hidePassword = true.obs;
   final TextEditingController emailUsername = TextEditingController();
   final TextEditingController password = TextEditingController();
   final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+
+  void login() async {
+    try {
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) return;
+
+      if (!loginFormKey.currentState!.validate()) return;
+
+      await AuthenticationRepository.instance
+          .loginWithEmailOrUsernameAndPassword(
+        emailUsername.text.trim(),
+        password.text.trim(),
+      );
+
+      Get.to(() => const NavigationMenu());
+    } catch (e) {
+      Loaders.dismissLoading();
+      Loaders.errorSnackBar(title: 'Error', message: e.toString());
+    }
+  }
 }
