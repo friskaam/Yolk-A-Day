d38a11d671fe849701613bdcdfcb47b78025690d
Friska Adisti Mahardini <friskamahardini@gmail.com>
2024-02-21 16:32:31 +0700

    feat: form validation & login controller

--- lib/authentication/screen/login.dart
+++ lib/authentication/screen/login.dart
@@ -1,137 +1,151 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:yolk_a_day/authentication/controllers/register_controller.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:yolk_a_day/authentication/controllers/login_controller.dart';
+import 'package:yolk_a_day/authentication/validators/validator.dart';
 
-import '/../../../navigation_menu.dart';
-
-class Login extends StatefulWidget {
+class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
-  State<Login> createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-
-  @override
   Widget build(BuildContext context) {
-    // final controller = ;
+    final controller = Get.put(LoginController());
     return Scaffold(
         body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 56),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 318,
-                  child: Stack(
-                    alignment: AlignmentDirectional.bottomCenter,
-                    children: [
-                      OverflowBox(
-                        maxWidth: 600,
-                        child: Container(
-                          width: double.infinity,
-                          height: double.infinity,
-                          decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(300),
-                                  bottomRight: Radius.circular(300)),
-                              color: Color.fromARGB(255, 248, 192, 56)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 56),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 318,
+              child: Stack(
+                alignment: AlignmentDirectional.bottomCenter,
+                children: [
+                  OverflowBox(
+                    maxWidth: 600,
+                    child: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(300),
+                              bottomRight: Radius.circular(300)),
+                          color: Color.fromARGB(255, 248, 192, 56)),
+                    ),
+                  ),
+                  const Positioned(
+                    bottom: 132,
+                    child: Text(
+                      "One step closer",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Outfit',
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 149,
+            ),
+            Form(
+                key: controller.loginFormKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: controller.emailUsername,
+                      validator: (value) => FormValidator.validateEmail(value),
+                      decoration: const InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 248, 192, 56),
+                            width: 3,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 248, 192, 56),
+                            width: 3,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        labelText: 'Username/Email',
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontFamily: 'Outfit',
+                          fontWeight: FontWeight.normal,
                         ),
                       ),
-                      const Positioned(
-                        bottom: 132,
-                        child: Text(
-                          "One step closer",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: 'Outfit',
-                              fontSize: 24,
-                              fontWeight: FontWeight.w600),
+                      style: const TextStyle(),
+                      // validator: ,
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Obx(
+                      () => TextFormField(
+                        controller: controller.password,
+                        obscureText: controller.hidePassword.value,
+                        decoration: InputDecoration(
+                          focusedBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(255, 248, 192, 56),
+                              width: 3,
+                              style: BorderStyle.solid,
+                            ),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(255, 248, 192, 56),
+                              width: 3,
+                              style: BorderStyle.solid,
+                            ),
+                          ),
+                          suffixIcon: IconButton(
+                            onPressed: () => controller.hidePassword.value = !controller.hidePassword.value,
+                              icon: Icon(controller.hidePassword.value ? Iconsax.eye_slash: Iconsax.eye),
+                          ),
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontFamily: 'Outfit',
+                            fontWeight: FontWeight.normal,
+                          ),
                         ),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 149,
-                ),
-                Form(
-                    // key: ,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          // controller: controller.emailUsername,
-                          decoration: const InputDecoration(
-                              focusedBorder: OutlineInputBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(5)),
-                                  borderSide: BorderSide(
-                                      color: Color.fromARGB(255, 248, 192, 56),
-                                      width: 3,
-                                      style: BorderStyle.solid)),
-                              enabledBorder: OutlineInputBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(5)),
-                                  borderSide: BorderSide(
-                                      color: Color.fromARGB(255, 248, 192, 56),
-                                      width: 3,
-                                      style: BorderStyle.solid)),
-                              labelText: 'Username/Email',
-                              labelStyle: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 15,
-                                  fontFamily: 'Outfit',
-                                  fontWeight: FontWeight.normal)),
-                          style: const TextStyle(),
-                          // validator: ,
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        TextFormField(
-                          // controller: ,
-                          decoration: const InputDecoration(
-                              focusedBorder: OutlineInputBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(5)),
-                                  borderSide: BorderSide(
-                                      color: Color.fromARGB(255, 248, 192, 56),
-                                      width: 3,
-                                      style: BorderStyle.solid)),
-                              enabledBorder: OutlineInputBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(5)),
-                                  borderSide: BorderSide(
-                                      color: Color.fromARGB(255, 248, 192, 56),
-                                      width: 3,
-                                      style: BorderStyle.solid)),
-                              labelText: 'Password',
-                              labelStyle: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 15,
-                                  fontFamily: 'Outfit',
-                                  fontWeight: FontWeight.normal)),
-                          style: const TextStyle(),
-                          // validator: ,
-                        ),
-                        const SizedBox(
-                          height: 47,
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor:
+                        style: const TextStyle(),
+                        // validator: ,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 47,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
                                   const Color.fromARGB(255, 248, 192, 56)),
-                              onPressed: () => Get.to(() => const NavigationMenu()),
-                              child: const Text('Log In')),
-                        )
-                      ],
-                    ))
-              ],
-            ),
-          ),
-        ));
+                          onPressed: () => controller.login(),
+                          child: const Text('Log In')),
+                    )
+                  ],
+                ))
+          ],
+        ),
+      ),
+    ));
   }
 }
