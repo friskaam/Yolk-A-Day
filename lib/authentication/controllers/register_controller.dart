a3ce6e13d17fe6569ca69d8c533969f2715e0b4d
Friska Adisti Mahardini <friskamahardini@gmail.com>
2024-02-21 16:30:52 +0700

    feat: register authentication

--- lib/authentication/controllers/register_controller.dart
+++ lib/authentication/controllers/register_controller.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:yolk_a_day/authentication/screen/welcome.dart';
+import 'package:yolk_a_day/data/repositories/auth/auth_repository.dart';
 import 'package:yolk_a_day/data/repositories/auth/network_manager.dart';
+import 'package:yolk_a_day/data/repositories/user_repository.dart';
 import 'package:yolk_a_day/loader/custom_loader.dart';
+import 'package:yolk_a_day/authentication/models/user_model.dart';
 
 class RegisterController extends GetxController {
   static RegisterController get instance => Get.find();
@@ -15,11 +19,8 @@ class RegisterController extends GetxController {
   final confirmPassword = TextEditingController();
   GlobalKey<FormState> registerFormKey = GlobalKey<FormState>(); // form key for form validation
 
-  Future<void> register() async {
+  void register() async {
     try {
-      // loader (start loading)
-      Loaders.showLoading();
-
       // check internet connectivity
       final isConnected = await NetworkManager.instance.isConnected();
       if(!isConnected) return;
@@ -27,11 +28,28 @@ class RegisterController extends GetxController {
       // form validation
       if(!registerFormKey.currentState!.validate()) return;
 
-      // final isConnected = await NetworkManager.instance.isConnected();
-    } catch (e) {
-      Loaders.errorSnackBar(title: 'Error', message: e.toString());
-    } finally {
+      // register user in firebase
+      final userCredential = await AuthenticationRepository.instance.registerWithEmailAndPassword(email.text.trim(), password.text.trim());
+
+      // save the user data in firebase firestore
+      final newUser = UserModel(
+        id: userCredential.user!.uid,
+        username: username.text.trim(),
+        email: email.text.trim(),
+        profilePicture: '',
+      );
+
+      final userRepository = Get.put(UserRepository());
+      await userRepository.saveUserRecord(newUser);
+
       Loaders.dismissLoading();
+      Loaders.successSnackBar(title: 'Congratulations', message: 'Your account has been created'!);
+
+      Get.to(() => const Welcome());
+
+    } catch (e) {
+      Loaders.dismissLoading();
+      Loaders.errorSnackBar(title: 'Error', message: e.toString());
     }
   }
 }
